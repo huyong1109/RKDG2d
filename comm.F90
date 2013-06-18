34aaa49c763899674556bb6653d3832ab9b4be63
huyong1109 <huyong1109@gmail.com>
2013-06-18 09:49:59 +0800

    local branch gpu

--- comm.F90
+++ comm.F90
@@ -2,7 +2,7 @@ module comm
 use kinds, only: long_kind
 use control, only: stepnow
 use domain_mod, only: ele
-use dimensions_mod, only: np,nc, nelem
+use dimensions_mod, only: np,nc, nelem_x, nelem_y
 implicit none 
 private 
 
@@ -11,14 +11,21 @@ contains
 subroutine bndy(tn) 
     
     integer, intent(in) :: tn
-    integer :: i, left
+    integer :: i, left, right, up, down
     !call stepnow(tn)
-    do i = 1, nelem
+    do i = 1, nelem_x
+        
 	if(i == 1) then 
             left = nelem
         else 
             left = i -1
         endif 
+        do j = 1, nelem_y
+	     if(j == 1) then 
+                 up = nelem
+             else 
+                 left = i -1
+             endif 
 	! exchange Fu and u boundary 
         ele(i)%Fhat(1) = ele(left)%Fu(np)
         ele(left)%Fhat(2) = ele(i)%Fu(1)
