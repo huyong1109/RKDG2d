0a35e3a639e334dd209ba23666364837f2793df0
huyong1109 <huyong1109@gmail.com>
2013-06-18 11:18:46 +0800

    neighour

--- comm.F90
+++ comm.F90
@@ -6,32 +6,23 @@ use dimensions_mod, only: np,nc, nelem_x, nelem_y
 implicit none 
 private 
 
+type :: neighbors
+        sequence 
+        integer :: 
 public :: bndy
 contains 
 subroutine bndy(tn) 
     
     integer, intent(in) :: tn
-    integer :: i, left, right, up, down
+    integer :: i,j, left, right, up, down
     !call stepnow(tn)
-    do i = 1, nelem_x
-        
-	if(i == 1) then 
-            left = nelem_x
-        else 
-            left = i -1
-        endif 
-        do j = 1, nelem_y
-	     if(j == 1) then 
-                 down  = nelem_y
-             else 
-                 down =  j -1
-             endif 
+
 	! exchange Fu and u boundary 
        ! ele(i,j)%Fhat(1) = ele(left)%Fu(np)
        ! ele(left)%Fhat(2) = ele(i)%Fu(1)
        ! ele(i)%vhat(1) = ele(left)%v(np, tn)
        ! ele(left)%vhat(2) = ele(i)%v(1, tn)
-        
+       end do         
     end do 
 end subroutine bndy 
 
--- domain_mod.F90
+++ domain_mod.F90
@@ -4,13 +4,12 @@ use dimensions_mod, only :  nelem_x, nelem_y
 use element_mod, only:  elem_t
   implicit none
   private
-  
+  ! types   
   type, public :: domain1D_t
      sequence
      integer :: start                     ! domain start index
      integer :: end                       ! domain end index
   end type domain1D_t
-
 
   type, public :: domain2D_t
      sequence
@@ -18,25 +17,56 @@ use element_mod, only:  elem_t
      type (domain1D_t) :: y
      integer :: nelem_lx, nelem_ly
   end type domain2D_t
+
+  type, private :: neighbors 
+      sequence 
+      integer :: left, right, up, down
+  end type neighors
+  ! variable   
   type(elem_t),allocatable, public :: ele(:,:)
   type(domain2D_t), public :: domain
+  type(neighbors), public :: neigh
 
   public :: decompose, init_domain
 contains 
-  function decompose(proc_x, proc_y, nproc) result(domain)
+  subroutine decompose(proc_x, proc_y, nproc, domain, neigh)
     
     ! input 
     integer, intent(in) :: proc_x, proc_y, nproc 
-    type(domain2D_t) :: domain
+    type(domain2D_t), intent(out):: domain
+    type(neighbors), intent(out) :: neigh
     
     ! local 
 
     integer :: lproc_x, lproc_y
     integer :: lelem_x, lelem_y
-
-    lproc_x = nproc/proc_x
-    lproc_y = mod(nproc,proc_x)
     
+
+    lproc_x = mod(nproc, proc_x)
+    lproc_y = nproc/proc_x
+
+    ! set neighbours  
+    if(lproc_x == 1 ) then 
+        neigh%left  = nproc + proc_x-1
+        neigh%right = nproc +1
+    else if(lproc_x == proc_x) then
+        neigh%left  = nproc -1
+        neigh%right = nproc - proc_x +1
+    else 
+        neigh%left  = nproc-1
+        neigh%right = nproc+1
+    endif 
+    if(lproc_y == 0) then 
+        neigh%up    = nproc + proc_x
+        neigh%down  = nproc + (proc_y-1)*proc_x
+    else if (j == proc_y-1) then
+        neigh%up    = nproc - (proc_y-1)*proc_x
+        neigh%down  = nproc - proc_x
+
+    else
+        neigh%up    =  nproc + proc_x
+        neigh%down  =  nproc - proc_x
+    endif 
 
     ! Local stuff , serial version 
     if (mod(nelem_x, proc_x) /= 0 .OR. mod(nelem_y, proc_y)  /= 0) then 
@@ -54,7 +84,7 @@ contains
         write(*,*) "  ys = ", domain%y%start, "  ye = ", domain%y%end
     endif 
     return 
-  end function decompose
+  end subroutine decompose
 
   subroutine init_domain(proc_x, proc_y)
     !for mpi
@@ -64,7 +94,7 @@ contains
     ! serial 
     myid = 1
     
-    domain = decompose(proc_x, proc_y, myid)
+    call decompose(proc_x, proc_y, myid, domain, neigh)
     !write(*,*) nelem_local
     allocate(ele(domain%nelem_lx, domain%nelem_ly))
 
